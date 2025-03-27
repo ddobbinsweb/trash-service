82f535b0b44dcaa648fe4e9c4a6b647f7bca8028
David Dobbins <ddobbinsweb@gmail.com>
2025-03-27 10:37:30 -0400

    add healthcheck

--- src/TrashService/Program.cs
+++ src/TrashService/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHealthChecks();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=trashservice.db"));
@@ -27,6 +28,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseHealthChecks("/health");
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
