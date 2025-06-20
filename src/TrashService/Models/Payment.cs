18535aa879d5b52ae160ef90f1c39e921b50fbcf
David Dobbins <ddobbins@gooddayfarm.com>
2025-06-20 16:29:00 -0400

    update db relationships

--- src/TrashService/Models/Payment.cs
+++ src/TrashService/Models/Payment.cs
@@ -1,10 +1,11 @@
 namespace TrashService.Models;
 
+[Index(nameof(Id))]
 public class Payment
 {
     public int Id { get; set; }
     public DateTime Date { get; set; }
     public int CustomerId { get; set; }
     public decimal Amount { get; set; } // Amount paid
-    public Customer Customer { get; set; } // Navigation property
+    public Customer Customer { get; set; } = null!; // Navigation property
 }
