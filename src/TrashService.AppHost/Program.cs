207736a2e428684a0e2d60337ed1c34e5cf5fa14
David Dobbins <ddobbinsweb@gmail.com>
2025-06-20 15:31:45 -0400

    Fix issue #6 (#7)
    * update service names
    
    * remove dockerfile
    
    * update nugets
    
    * move css and fix services
    
    * fix services to load
    
    * add styling
    
    * fix add services page
    
    * fix saving payment
    
    added styling and back
    
    * add styling
    
    * add default when no data to show

--- src/TrashService.AppHost/Program.cs
+++ src/TrashService.AppHost/Program.cs
@@ -1,11 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("trash-services")
-                      .WithDataVolume(isReadOnly: false);
+var postgres = builder.AddPostgres("postgres")
+    .WithPgAdmin()
+    .WithDataVolume(isReadOnly: false);
 
 var db = postgres.AddDatabase("trash-services-db");
 
-builder.AddProject<Projects.TrashService>("trashservice")
+builder.AddProject<Projects.TrashService>("trashservice-web")
     .WithReference(db)
     .WaitFor(db);
 
